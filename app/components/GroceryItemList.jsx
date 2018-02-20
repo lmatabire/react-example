41f1d0d0af99d2fe4c4d7d514c5ffe72db7ca644
lmatabire <matabire23@gmail.com>
2018-02-20 08:54:43 -0600

    grocery item component

---
+++ app/components/GroceryItemList.jsx
@@ -0,0 +1,22 @@
+var React = require('react');
+var createReactClass = require('create-react-class');
+var GroceryItem = require('./GroceryItem.jsx')
+
+module.exports = createReactClass ({
+    render: function() {
+        return(
+            <div>
+                <h1>Grocery Listify</h1>
+                <div>
+                    {
+                        this.props.items.map(function(item, index){
+                            return(
+                                <GroceryItem item={item} key={"item" + index} />
+                            )
+                        })
+                    }
+                </div>
+            </div>
+        )
+    }
+})
---
+++ app/main.jsx
@@ -0,0 +1,20 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+console.log("Hello from JSX!");
+
+
+var GroceryItemList = require('./components/GroceryItemList.jsx');
+
+var initial = [{
+    name: "Ice Cream"
+}, {
+    name: "Waffles"
+},{
+    name: "Candy",
+    purchased: true
+}, {
+    name: "Snarks"
+}];
+
+ReactDOM.render(<GroceryItemList items={initial} />,app)
